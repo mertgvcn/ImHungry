11faeb9cddc9a18380207276815a842eab2d7dfb
mert_gvcn <mertguvercin2942@gmail.com>
2024-03-07 15:24:52 +0300

    category edit name added

--- src/pages/RestaurantOwner/Menu/components/CategoryItemCard.tsx
+++ src/pages/RestaurantOwner/Menu/components/CategoryItemCard.tsx
@@ -12,7 +12,7 @@ const CategoryItemCard = (props: CategoryItemCardType) => {
     const [editState, setEditState] = useState(false)
 
     const isEditOpen = () => {
-        if(!editState) {
+        if (!editState) {
             return (<i className="fa-regular fa-pen-to-square"></i>)
         }
         else {
@@ -21,16 +21,29 @@ const CategoryItemCard = (props: CategoryItemCardType) => {
     }
 
     return (
-        <div className='category-item-card-container'>
-            <div className='category-name'>
-                <i className="fa-regular fa-circle"></i>
-                <p>{props.categoryInfo.Name}</p>
+        <>
+            <div className='category-item-card-container'>
+                <div className='category-name'>
+                    <i className="fa-regular fa-circle"></i>
+                    <p>{props.categoryInfo.Name}</p>
+                </div>
+
+                <div className='category-edit-button' onClick={() => setEditState(!editState)}>
+                    {isEditOpen()}
+                </div>
             </div>
 
-            <div className='category-edit-button' onClick={() => setEditState(!editState)}>
-                {isEditOpen()}
-            </div>
-        </div>
+            {editState &&
+                <div className='category-edit-container'>
+                    <div className='edit-name-wrapper'>
+                        <p>Name: </p>
+                        <input className="name-input" type="text" value={props.categoryInfo.Name} />
+                        <i className="fa-solid fa-circle-check"></i>
+                    </div>
+                </div>
+            }
+        </>
+
     )
 }
 
